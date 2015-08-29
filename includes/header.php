3736430545d51ad7f06159213341a4283599ba4c
Ar1k <arunkmr492@gmail.com>
2015-08-29 16:31:05 +0530

    Added preloader

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,18 @@
 <!-- Preloader -->
-<!-- <div id="preloader">
+<script type="text/javascript">
+    //<![CDATA[
+    $(window).load(function () { // makes sure the whole site is loaded
+        $('#status').fadeOut(); // will first fade out the loading animation
+        $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+        $('body').delay(350).css({
+            'overflow': 'visible'
+        });
+    })
+     //]]>
+</script>
+<div id="preloader">
     <div id="status">&nbsp;</div>
-</div> -->
+</div>
 <!--preloader ends	-->
 <div class="top_bar col-xs-12 no-pad z-top easing">
   <div class="container-fluid">
