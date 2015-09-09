155d0eb480ed94891c58bc7cd84f13e267ef3c1b
Ar1k <arunkmr492@gmail.com>
2015-09-09 19:02:56 +0530

    minor update

--- includes/header.php
+++ includes/header.php
@@ -1,16 +1,3 @@
-<!-- Preloader -->
-<script type="text/javascript">
-    //<![CDATA[
-    $(window).load(function () { // makes sure the whole site is loaded
-        $('#status').fadeOut(); // will first fade out the loading animation
-        $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
-        $('body').delay(350).css({
-            'overflow': 'visible'
-        });
-        landing_page_anim();
-    })
-     //]]>
-</script>
 <div id="preloader">
     <div id="status">&nbsp;</div>
 </div>
